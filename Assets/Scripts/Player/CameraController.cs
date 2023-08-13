eb79ba065bb9e689fba22a88517b9453d9e38dbe
lipikansal <kansallipi@gmail.com>
2023-08-14 00:10:08 +0530

    completed

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target; //player transform
+    private Vector3 offset; //offset btw player and camera
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - target.position;//initialise offset
+    }
+
+    void LateUpdate()//camera will follow player
+    {
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + target.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, 0.6f);//go from transformed to new position
+    }
+}
---
+++ Assets/Scripts/Tiles/TileManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    private List<GameObject> activeTiles; //tiles being used
+    public GameObject[] tilePrefabs;
+
+    public float tileLength = 30;
+    public int numberOfTiles = 3;
+    public int totalNumOfTiles = 8;
+
+    public float zSpawn = 0;//zposition of spawn, x and y not changing
+
+    private Transform playerTransform;
+
+    private int previousIndex;
+
+    void Start()
+    {
+        activeTiles = new List<GameObject>();
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            if(i==0)//first tile needs to be without hurdle
+                SpawnTile();
+            else
+                SpawnTile(Random.Range(0, totalNumOfTiles)); //to randomize the order of tiles
+        }
+
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+    void Update()
+    {
+        if(playerTransform.position.z - 30 >= zSpawn - (numberOfTiles * tileLength))
+        {
+            int index = Random.Range(0, totalNumOfTiles);//spawn new randomtile
+            while(index == previousIndex)
+                index = Random.Range(0, totalNumOfTiles);
+
+            DeleteTile(); //need to delete tiles which are behind the player
+            SpawnTile(index);
+        }
+            
+    }
+
+    public void SpawnTile(int index = 0)
+    {
+        GameObject tile = tilePrefabs[index];
+        if (tile.activeInHierarchy)
+            tile = tilePrefabs[index + 8];
+
+        if(tile.activeInHierarchy)
+            tile = tilePrefabs[index + 16];
+
+        tile.transform.position = Vector3.forward * zSpawn;
+        tile.transform.rotation = Quaternion.identity;
+        tile.SetActive(true);
+
+        activeTiles.Add(tile);
+        zSpawn += tileLength; //next tile
+        previousIndex = index; //append the tiles to list
+    }
+
+    private void DeleteTile()
+    {
+        activeTiles[0].SetActive(false);
+        activeTiles.RemoveAt(0); //remove tiles from list
+        PlayerManager.score += 3;
+    }
+}
